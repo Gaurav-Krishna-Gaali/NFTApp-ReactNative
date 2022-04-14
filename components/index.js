832d13fa55fb97f7493a018524b8b9ebd8bb3a1d
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-04-14 12:38:56 +0530

    Layout Check ✔️

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStautsBar from "./FocusedStautsBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+
+export { HomeHeader, NFTCard, FocusedStautsBar, CircleButton, RectButton };
